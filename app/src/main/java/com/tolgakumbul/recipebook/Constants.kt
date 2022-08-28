ef1b8478cfe2260e58e29caa371ef6e4a64414f5
Tolga Kumbul <61317042+TolgaKmbl@users.noreply.github.com>
2022-08-28 20:52:15 +0300

    select image from storage and show

---
+++ app/src/main/java/com/tolgakumbul/recipebook/Constants.kt
@@ -0,0 +1,6 @@
+package com.tolgakumbul.recipebook
+
+object Constants{
+        const val REQUEST_CODE_ONE = 1
+        const val REQUEST_CODE_TWO = 2
+    }
--- app/src/main/java/com/tolgakumbul/recipebook/DetailFragment.kt
+++ app/src/main/java/com/tolgakumbul/recipebook/DetailFragment.kt
@@ -1,13 +1,26 @@
 package com.tolgakumbul.recipebook
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
+
+    var pickedImage: Uri? = null;
+    var pickedBitmap : Bitmap? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +43,66 @@ class DetailFragment : Fragment() {
         }
     }
 
-    private fun addImage(view:View){
-
+    private fun addImage(view: View) {
+        activity?.let {
+            if (ContextCompat.checkSelfPermission(
+                    it.applicationContext,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                //Get permission
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    Constants.REQUEST_CODE_ONE
+                )
+            } else {
+                //Permission granted already
+                val galleryIntent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galleryIntent, Constants.REQUEST_CODE_TWO)
+            }
+        }
     }
 
-    private fun saveRecipe(view:View){
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == Constants.REQUEST_CODE_ONE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val galleryIntent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galleryIntent, Constants.REQUEST_CODE_TWO)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Constants.REQUEST_CODE_TWO && resultCode == Activity.RESULT_OK && data != null) {
+            pickedImage = data.data
+            try {
+                context?.let {
+                    if (pickedImage != null) {
+                        if(Build.VERSION.SDK_INT >= 28){
+                            val source = ImageDecoder.createSource(it.contentResolver, pickedImage!!)
+                            pickedBitmap = ImageDecoder.decodeBitmap(source)
+                            addImageLogo.setImageBitmap(pickedBitmap)
+                        } else {
+                            pickedBitmap = MediaStore.Images.Media.getBitmap(it.contentResolver, pickedImage)
+                            addImageLogo.setImageBitmap(pickedBitmap)
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun saveRecipe(view: View) {
 
     }
 
